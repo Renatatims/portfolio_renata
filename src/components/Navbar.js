b8d295a975c6ceac60785c02c996272045854ec0
ReTims <renatatims@gmail.com>
2022-12-16 16:03:27 -0600

    Added bootstrap package + Navbar additions using bootstrap classes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,67 @@
-import React from 'react';
-import '../style/Navbar.css';
-import '../style/style.css';
+import React from "react";
+import "../style/Navbar.css";
+import "../style/style.css";
 
 function Navbar() {
-    return (
-      <nav class="navigation" id="nav">
-              <a href="#nav">home</a>
-              <a href="#contact">contact</a>
-              <a href="#aboutme">about me </a>
-              <div class="dropdown">
-                  <button class="dropbtn">
-                      <a href="#projects">projects</a>
-                      <i class="down"></i>
-                  </button>
-                  <div class="dropdown-content">
-                      <a href="#project1">Project 1</a>
-                      <a href="#project2">Project 2</a>
-                      <a href="#project3">Project 3</a>
-                      <a href="#project4">Project 4</a>
-                      <a href="#project5">Project 5</a>
-                  </div>
-              </div>
-          </nav>
-    
-    );
-  }
-  
-  export default Navbar;
+  return (
+    <nav class="navigation" id="nav">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">
+            home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#aboutme">
+            about me
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">
+            contact
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            data-bs-toggle="dropdown"
+            href="#projects"
+            role="button"
+            aria-expanded="false"
+          >
+            projects
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="#project1">
+                Project 1
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#project2">
+                Project 2
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#project3">
+                Project 3
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#project4">
+                Project 4
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#project5">
+                Project 5
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
