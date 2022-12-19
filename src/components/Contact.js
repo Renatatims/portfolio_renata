7f88e8d29405fbccf735175bcb612ae5f273e695
ReTims <renatatims@gmail.com>
2022-12-19 17:19:09 -0600

    Added fontawesome icons + fontawesome brands icons installed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import '../style/style.css';
+import ReactDOM from 'react-dom';
+import '../style/Contact.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faPhone, faEnvelope} from  "@fortawesome/free-solid-svg-icons"
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
 
 
 function Contact() {
@@ -11,6 +15,25 @@ function Contact() {
         </div>
 
         <h3>Contact me:</h3>
+        <ul class="contact-icons">
+            <li class="icons"> 
+                <a><FontAwesomeIcon icon={faPhone} size= "3x"/></a>
+                <p>000-XXX-XXXX</p>
+            </li>
+            <li class="icons"> 
+                <a href="https://github.com/Renatatims" target="_blank"> <FontAwesomeIcon icon={faEnvelope} size= "3x"/></a>
+                <p>@gmail.com</p>
+                
+            </li>
+            <li class="icons">
+                <a href="https://github.com/Renatatims" target="_blank"><FontAwesomeIcon icon={faGithub} size= "3x" /></a>
+                
+            </li>
+            <li class="icons">
+                <a href="https://www.linkedin.com/"  target="_blank"><FontAwesomeIcon icon={faLinkedin} size= "3x"  /></a>
+                
+            </li>
+        </ul>
         </section>
     );
 }
