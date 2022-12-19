413cf1d640c3c7846fd9772c092d0cfcb7672f91
ReTims <renatatims@gmail.com>
2022-12-19 15:53:38 -0600

    Added more projects to projects page + preview project Images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,94 @@
 import React from "react";
 import "../style/style.css";
+import "../style/Projects.css";
 
 function Projects() {
   return (
-    <section id = "projects">
-    <div class="row">
-      <div class="col">
-      <div class="card" style={{ width: "18rem" }}>
-        <img
-          src={require("../assets/cardImages/card0Preview.PNG")}
-          class="card-img-top"
-          alt="..."
-        >
-        </img>
-        <div class="overlay-card">
+    <section class="container" id="projects">
+      <div class="row">
+        <div class="col">
+          <div class="card" style={{ width: "30rem" }}>
+            <img
+              src={require("../assets/cardImages/card0Preview.PNG")}
+              class="card-img-top"
+              alt="..."
+            ></img>
+            <div class="overlay-card"></div>
+            <div class="card-body">
+              <p class="card-text">GitHelpNOW!</p>
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-          <p class="card-text">
-          GitHelpNOW!
-          </p>
+
+        <div class="col">
+          <div class="card" style={{ width: "30rem" }}>
+            <img
+              src={require("../assets/cardImages/card1Preview.PNG")}
+              class="card-img-top"
+              alt="..."
+            ></img>
+            <div class="overlay-card"></div>
+            <div class="card-body">
+              <p class="card-text">AccountabiliBuddies</p>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
-      
-      <div class="col">
-      <div class="card" style={{ width: "18rem" }}>
-        <img
-          src={require("../assets/cardImages/card1Preview.PNG")}
-          class="card-img-top"
-          alt="..."
-        >
-        </img>
-        <div class="overlay-card">
+
+        <div class="col">
+          <div class="card" style={{ width: "30rem" }}>
+            <img
+              src={require("../assets/cardImages/card2Preview.PNG")}
+              class="card-img-top"
+              alt="..."
+            ></img>
+            <div class="overlay-card"></div>
+            <div class="card-body">
+              <p class="card-text">AccountabiliBuddies</p>
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-          <p class="card-text">
-          AccountabiliBuddies
-          </p>
+
+        <div class="col">
+          <div class="card" style={{ width: "30rem" }}>
+            <img
+              src={require("../assets/cardImages/card3Preview.PNG")}
+              class="card-img-top"
+              alt="..."
+            ></img>
+            <div class="overlay-card"></div>
+            <div class="card-body">
+              <p class="card-text">AccountabiliBuddies</p>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
+
+        <div class="col">
+          <div class="card" style={{ width: "30rem" }}>
+            <img
+              src={require("../assets/cardImages/card4Preview.PNG")}
+              class="card-img-top"
+              alt="..."
+            ></img>
+            <div class="overlay-card"></div>
+            <div class="card-body">
+              <p class="card-text">AccountabiliBuddies</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card" style={{ width: "30rem" }}>
+            <img
+              src={require("../assets/cardImages/card5Preview.PNG")}
+              class="card-img-top"
+              alt="..."
+            ></img>
+            <div class="overlay-card"></div>
+            <div class="card-body">
+              <p class="card-text">AccountabiliBuddies</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
