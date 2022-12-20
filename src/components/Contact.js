340fc0efb46cf2f6351dfb7e07c5a942052f40c1
ReTims <renatatims@gmail.com>
2022-12-20 11:37:11 -0600

    Created utils folder with helpers.js document to validate user's email input + added a form to ontaCct.js + basic CSS styling for the form and icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../style/Contact.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faPhone, faEnvelope} from  "@fortawesome/free-solid-svg-icons"
-import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
-
+//import React from 'react';
+import ReactDOM from "react-dom";
+import "../style/Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+//Helper to validate the user's email input
+import { validateEmail } from "../utils/helpers";
 
 function Contact() {
-    return (
-        <section id="contact" class="section">
-        <h2 id="contact-name"> <a href="#nav"> Renata Rondon </a> </h2>
-        <div id="contact-line">
-
+  return (
+    <section id="contact" class="section">
+      <h2 id="contact-name">
+        {" "}
+        <a href="#nav"> Renata Rondon </a>{" "}
+      </h2>
+      <div id="contact-line"></div>
+      <h3>Contact me:</h3>
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          ></input>
         </div>
-
-        <h3>Contact me:</h3>
-        <ul class="contact-icons">
-            <li class="icons"> 
-                <a><FontAwesomeIcon icon={faPhone} size= "3x"/></a>
-                <p>000-XXX-XXXX</p>
-            </li>
-            <li class="icons"> 
-                <a href="https://github.com/Renatatims" target="_blank"> <FontAwesomeIcon icon={faEnvelope} size= "3x"/></a>
-                <p>@gmail.com</p>
-                
-            </li>
-            <li class="icons">
-                <a href="https://github.com/Renatatims" target="_blank"><FontAwesomeIcon icon={faGithub} size= "3x" /></a>
-                
-            </li>
-            <li class="icons">
-                <a href="https://www.linkedin.com/"  target="_blank"><FontAwesomeIcon icon={faLinkedin} size= "3x"  /></a>
-                
-            </li>
-        </ul>
-        </section>
-    );
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name"
+          ></input>
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Example textarea</label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <button type="submit" class="btn btn-secondary mb-2">Submit</button>
+      </form>
+      
+      <ul class="contact-icons" >
+        <li class="icons">
+          <a>
+            <FontAwesomeIcon icon={faPhone} size="2x" />
+          </a>
+        </li>
+        <li class="icons">
+          <a href="https://github.com/Renatatims" target="_blank">
+            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+          </a>
+        </li>
+        <li class="icons">
+          <a href="https://github.com/Renatatims" target="_blank">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+        </li>
+        <li class="icons">
+          <a href="https://www.linkedin.com/" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+        </li>
+      </ul>
+     
+    </section>
+  );
 }
 export default Contact;
