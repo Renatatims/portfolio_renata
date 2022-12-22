082c0b1ce75746edcc521bde94cfd5fa5b4aa568
ReTims <renatatims@gmail.com>
2022-12-22 13:34:23 -0600

    Updated projects links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,11 +8,11 @@ function Projects() {
       <div className="row" id="projectsDiv">
         <div className="col" id="projectCard">
           <div className="card" style={{ width: "30rem" }}>
-            <img
+          <a href="https://renatatims.github.io/GitHelpNOW/index.html" target="_blank"><img
               src={require("../assets/cardImages/card0Preview.PNG")}
               className="card-img-top"
               alt="..."
-            ></img>
+            ></img></a>
             <div className="overlay-card"></div>
             <div className="card-body">
               <p className="card-text">GitHelpNOW!</p>
@@ -22,11 +22,11 @@ function Projects() {
 
         <div className="col" id="projectCard">
           <div className="card" style={{ width: "30rem" }}>
-            <img
+          <a href="https://github.com/Renatatims/AccountabiliBuddies" target="_blank"><img
               src={require("../assets/cardImages/card1Preview.PNG")}
               className="card-img-top"
               alt="..."
-            ></img>
+            ></img></a>
             <div className="overlay-card"></div>
             <div className="card-body">
               <p className="card-text">AccountabiliBuddies</p>
@@ -36,56 +36,56 @@ function Projects() {
 
         <div className="col" id="projectCard">
           <div className="card" style={{ width: "30rem" }}>
-            <img
+          <a href=" https://github.com/Renatatims/note_taker" target="_blank"><img
               src={require("../assets/cardImages/card2Preview.PNG")}
               className="card-img-top"
               alt="..."
-            ></img>
+            ></img></a>
             <div className="overlay-card"></div>
             <div className="card-body">
-              <p className="card-text">AccountabiliBuddies</p>
+              <p className="card-text">Note Taker</p>
             </div>
           </div>
         </div>
 
         <div className="col" id="projectCard">
           <div className="card" style={{ width: "30rem" }}>
-            <img
+          <a href="https://renatatims.github.io/weather_dashboard/" target="_blank"><img
               src={require("../assets/cardImages/card3Preview.PNG")}
               className="card-img-top"
               alt="..."
-            ></img>
+            ></img></a>
             <div className="overlay-card"></div>
             <div className="card-body">
-              <p className="card-text">AccountabiliBuddies</p>
+              <p className="card-text">Weather Dashboard</p>
             </div>
           </div>
         </div>
 
         <div className="col" id="projectCard">
           <div className="card" style={{ width: "30rem" }}>
-            <img
+          <a href="https://github.com/Renatatims/social_networkAPI" target="_blank"><img
               src={require("../assets/cardImages/card4Preview.PNG")}
               className="card-img-top"
               alt="..."
-            ></img>
+            ></img></a>
             <div className="overlay-card"></div>
             <div className="card-body">
-              <p className="card-text">AccountabiliBuddies</p>
+              <p className="card-text">Social Network API</p>
             </div>
           </div>
         </div>
 
         <div className="col" id="projectCard">
           <div className="card" style={{ width: "30rem" }}>
-            <img
+          <a href="https://renata-jate-text-editor.herokuapp.com/" target="_blank"><img
               src={require("../assets/cardImages/card5Preview.PNG")}
               className="card-img-top"
               alt="..."
-            ></img>
+            ></img></a>
             <div className="overlay-card"></div>
             <div className="card-body">
-              <p className="card-text">AccountabiliBuddies</p>
+              <p className="card-text">Text Editor JATE</p>
             </div>
           </div>
         </div>
