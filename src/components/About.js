41cb8cc2daa62b23b795a2b8ddf1715a32b1fe38
ReTims <renatatims@gmail.com>
2023-01-25 13:02:27 -0600

    Updated description in About page

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,10 @@ function About() {
     <div className="aboutMe">
       <h1 id="aboutTitle">About Page</h1>
       <p id="aboutText">
-        Hello, I am Renata Rondon, Full-Stack Web developer. I have a Bachelor's
+        Hello, I am Renata Rondon, Full-Stack Web Developer. I have a Bachelor's
         in Environmental Engineering and for the past 4 years I have worked as a
-        Purchasing Manager for a cruise company. I am currently enrolled in UC
-        Berkeley Full- Stack coding bootcamp and I am looking for oportunities
-        to start my new path as software developer.
+        Purchasing Manager for a cruise company. I recently graduated in Full Stack Web Development from UC
+        Berkeley and I am looking for oportunities to start my new path as a Software Developer. 
       </p>
       <img
         id="profilepic"
