d8adab899de3a516a1ebef4e0f3de06b1a9800c7
ReTims <renatatims@gmail.com>
2023-03-09 11:18:19 -0600

    Added structure to the Modal - created 3 buttons so user can access the deployed application and github repo from the modal

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 
 function ProjectModal(props) {
   return (
-    <Modal
-      show={props.show}
-      onHide={props.handleClose}
-      size="md"
-      centered
-    >  
-    <Modal.Title>Title</Modal.Title>
+    <Modal 
+    show={props.show} 
+    onHide={props.handleClose} 
+    size="md" 
+    centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Title</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Description</Modal.Body>
+      <Modal.Body>Technologies</Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" href="/" target="_blank">
+          Visit App
+        </Button>
+        <Button variant="primary" href="/" target="_blank">
+          Github Repo
+        </Button>
+        <Button variant="secondary" onClick={props.handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 }
