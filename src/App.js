c1645bbd20a37b150f6c8dadde612a92afea529b
ReTims <renatatims@gmail.com>
2023-03-09 22:54:33 -0600

    Updates

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import PortfolioPages from "./components/PortfolioPages";
-
-const App = () => <PortfolioPages />;
-export default App;
--- src/utils/helpers.js
+++
@@ -1,5 +0,0 @@
-//Regex to validate e-mail
-export function validateEmail(email) {
-  const re = /^([a-z0-9_.-]+)@([\da-z.-]+)\.([a-z.]{2,6})$/;
-  return re.test(String(email).toLowerCase());
-}
